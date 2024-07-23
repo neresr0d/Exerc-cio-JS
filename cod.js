48e6264629f5416e2fe270b5c74089f16a641d20
neresr0d <rodrigoneres030@gmai.com>
2024-07-23 19:29:18 -0300

    exercicios JS

---
+++ cod.js
@@ -0,0 +1,23 @@
+//Questão 01
+
+console.log("Questão 01")
+
+
+//Questão 02
+const cores = ['azul', 'vermelho', 'verde', 'preto']
+console.log(cores[0]) 
+
+
+//Questão 3
+
+var frutas = ['maça', 'banana', 'laranja', 'uva']
+console.log(frutas[2])
+
+//Questão 04
+
+function areaQuadrado(lado) {
+    console.log(`A área de um quadrado de lado ${lado}m é ${lado * lado}m2`)
+} areaQuadrado(2)
+
+//Questão 05
+
